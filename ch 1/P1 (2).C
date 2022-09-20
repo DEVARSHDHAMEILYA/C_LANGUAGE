31641e7f1b67b5c8c9316d5c171f40d6bfae96f4
DEVARSHDHAMEILYA <113960504+DEVARSHDHAMEILYA@users.noreply.github.com>
2022-09-20 09:43:22 +0530

    Add files via upload

---
+++ ch 1/P1 (2).C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("WAP to print your Name   : Patel Devarsh");
+	printf("\nWAP to print your Age    : 18");
+	printf("\nWEP to print your School : R&W");
+
+	getch();
+}
---
+++ ch 1/P3.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("\n -  -  -");
+	printf("\n|	|");
+	printf("\nN	|");
+	printf("\nA	|");
+	printf("\nM	|");
+	printf("\nE	|");
+	printf("\n|	|");
+	printf("\n -  -  -");
+
+
+	getch();
+}
